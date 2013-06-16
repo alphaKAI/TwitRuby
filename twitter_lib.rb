6a2c5117005fe3ca0740894e0e3bd4a77fd885f8
alphaKAI <alpha.kai.net@alpha-kai-net.info>
2013-06-16 21:09:26 +0900

    ちょっといじった

--- twitter_lib.rb
+++ twitter_lib.rb
@@ -5,6 +5,7 @@
 # Copyright (C) alphaKAI @alpha_kai_NET 2013 http://alpha-kai-net.info #
 # GPLv3 LICENSE                                                        #
 #                                                                      #
+# Version:0.0.1_alpha                                                  #
 # This is one of the Twitter Library                                   #
 # inspired by https://gist.github.com/pnlybubbles/5476370              #
 # This is development version.                                         #
@@ -51,8 +52,8 @@ class TwitRuby
 		access_token,
 		access_token_secret
 		)
-		# puts access_token
-		# puts access_token_secret
+		puts access_token
+		puts access_token_secret
 	end#end of function
 	
 	def oauth_init
@@ -70,8 +71,8 @@ class TwitRuby
 			puts ""#改行
 
 			access_token = request_token.get_access_token(
-			:oauth_token => request_token.token,
-			:oauth_verifier => pin
+			"oauth_token" => request_token.token,
+			"oauth_verifier" => pin
 			)
 
 			access_tokens = []
@@ -87,22 +88,22 @@ class TwitRuby
 	def update(str, id="")
 		if (id.empty?) then
 			@access_token.post("/1.1/statuses/update.json",
-								:status =>str)
+			"status" =>str)
 		else
 			@access_token.post("/1.1/statuses/update.json",
-								:status =>str,
-								:in_reply_status_id =>id.to_s)
+			"status" =>str,
+			"in_reply_status_id" =>id.to_s)
 		end
 	end
 	
 	def favorite(id)
 		@access_token.post("/1.1/favorites/create.json",
-							:id => id.to_s)
+		"id" => id.to_s)
 	end
  
 	def unfavorite(id)
 		@access_token.post("/1.1/favorites/destroy.json",
-							:id => id.to_s)
+		"id" => id.to_s)
 	end
  
 	def retweet(id)
@@ -118,54 +119,53 @@ class TwitRuby
 	end
 	
 	def mentions_timeline(count="",since_id="",max_id="",trim_user="",contributor_details="",include_entities="")
-		return JSON.parse(
-							(@access_token.get("/1.1/statuses/mentions_timeline.json",
-								:count => count,
-								:since_id => since_id,
-								:max_id => max_id,
-								:trim_user => trim_user,
-								:contributor_details=>contributor_details,
-								:include_entities =>include_entities)
+		return JSON.parse((@access_token.get("/1.1/statuses/mentions_timeline.json",
+						"count" => count,
+						"since_id" => since_id,
+						"max_id" => max_id,
+						"trim_user" => trim_user,
+						"contributor_details" =>contributor_details,
+						"include_entities" =>include_entities)
 						).body)
 	end
 	
 	#GET statuses/home_timeline
 	def home_timeline(count,since_id="",max_id="",trim_user="",include_entities="",include_user_entities="")
-		return Json.parse(@access_token.get("/1.1/statuses/home_timeline.json",
-											:count =>count,
-											:since_id => since_id,
-											:max_id => max_id,
-											:trim_user => trim_user,
-											:include_entities => include_entities,
-											:include_user_entities => include_user_entities
-											).body)
+		return JSON.parse(@access_token.get("/1.1/statuses/home_timeline.json",
+						"count" =>count,
+						"since_id" => since_id,
+						"max_id" => max_id,
+						"trim_user" => trim_user,
+						"include_entities" => include_entities,
+						"include_user_entities" => include_user_entities
+						).body)
 	end
 	
 	#GET statuses/user_timeline
-	def user_timeline 
-		return Json.parse(@access_token.get("/1.1/statuses/user_timeline.json",
-											:user_id => user_id,
-											:screen_name => screen_name,
-											:since_id => since_id,
-											:count => count,
-											:max_id => max_id,
-											:trim_user => trim_user,
-											:exclude_replies => exclude_replies,
-											:contributor_details => contributor_details,
-											:include_rts => include_rts
-											).body)
+	def user_timeline(user_id="",screen_name="",since_id="",count="",max_id="",trim_user="",exclude_replies="",contributor_details="",include_rts="")
+		return JSON.parse(@access_token.get("/1.1/statuses/user_timeline.json",
+						"user_id" => user_id,
+						"screen_name" => screen_name,
+						"since_id" => since_id,
+						"count" => count,
+						"max_id" => max_id,
+						"trim_user" => trim_user,
+						"exclude_replies" => exclude_replies,
+						"contributor_details" => contributor_details,
+						"include_rts" => include_rts
+						).body)
 	end
 	
 	#GET statuses/retweets_of_me
 	def get_rom(count,since_id="",max_id="",trim_user="",include_entities="",include_user_entities="")
-		return Json.parse(@access_token.get("/1.1/statuses/retweets_of_me.json",
-											:count =>count,
-											:since_id => since_id,
-											:max_id => max_id,
-											:trim_user => trim_user,
-											:include_entities => include_entities,
-											:include_user_entities => include_user_entities
-											).body)
+		return JSON.parse(@access_token.get("/1.1/statuses/retweets_of_me.json",
+						"count" =>count,
+						"since_id" => since_id,
+						"max_id" => max_id,
+						"trim_user" => trim_user,
+						"include_entities" => include_entities,
+						"include_user_entities" => include_user_entities
+						).body)
 	end
 	
 	##########################################################################
@@ -176,9 +176,9 @@ class TwitRuby
 			return "Error. user_id => empty"
 		end
 		@access_token.get("/1.1/direct_messages/new.json",
-							:user_id => user_id,
-							:screen_name => screen_name,
-							:text => text)
+		"user_id" => user_id,
+		"screen_name" => screen_name,
+		"text" => text)
 	end
 	
 	#GET direct_messages/sent
@@ -187,33 +187,43 @@ class TwitRuby
 			return "Error. same parms is empty"
 		end
 		
-		return Json.parse(@access_token.get("/1.1/direct_messages/sent.json",
-											:since_id => since_id,
-											:max_id => max_id,
-											:count => count,
-											:page => page,
-											:include_entities => include_entities,
-											).body)
+		return JSON.parse(@access_token.get("/1.1/direct_messages/sent.json",
+						"since_id" => since_id,
+						"max_id" => max_id,
+						"count" => count,
+						"page" => page,
+						"include_entities" => include_entities,
+						).body)
 	end
 	
 	#GET direct_messages
 	def dm_msgs(since_id="",max_id="",count="",include_entities="",skip_status="")
-		return Json.parse(@access_token.get("/1.1/direct_messages.json",
-											:since_id => since_id,
-											:max_id => max_id,
-											:count => count,
-											:include_entities => include_entities,
-											:skip_status => skip_status
-											).body)
+		return JSON.parse(@access_token.get("/1.1/direct_messages.json",
+						"since_id" => since_id,
+						"max_id" => max_id,
+						"count" => count,
+						"include_entities" => include_entities,
+						"skip_status" => skip_status
+						).body)
 	end
 	
 	#POST direct_messages/destroy
 	def post_dm_destory(id,include_entities="")
 		@access_token.post("/1.1/direct_messages/destroy.json",
-							:id => id,
-							:include_entities => include_entities)
+		"id" => id,
+		"include_entities" => include_entities)
 	end
 	
 	##########################################################################
 	
+	#GET followers/ids
+	def follower_ids(user_id="",screen_name="",cursor="",stringify_ids="",count="")
+		return JSON.parse(@access_token.get("/1.1/followers/ids.json",
+						"user_id" => user_id,
+						"screen_name" => screen_name,
+						"cursor" => cursor,
+						"stringify_ids" => stringify_ids,
+						"count" => count
+						).body)
+	end
 end#End of Class TwitRuby
